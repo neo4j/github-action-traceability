b4ec3cf68c618daf42a8bc65625d0d8c2b4bd0fd
Daniel Leaver <daniel.leaver@neo4j.com>
2023-03-13 16:31:09 +0000

    [NOID] Touchups

--- src/errors.ts
+++ src/errors.ts
@@ -13,9 +13,9 @@ export const ERR_INVALID_NOID = (shortLinkId: string) =>
 export const ERR_INVALID_INPUT = (setting: string, value: string) =>
   `Unrecognised value ${value} for "${setting}".`;
 export const ERR_NO_MATCHING_ATTACHMENT = (commentUrl: string, shortLink: string, prUrl: string) =>
-  `Although the comment ${commentUrl} contained the link https://trello.com/c/${shortLink}, when checking the Trello card we could not find an attachment for pull request ${prUrl}. This can be due to a genuine user error on your behalf, if so then please attach ${prUrl} to https://trello.com/c/${shortLink}. In rarer cases, this step may also fail when a comment in a pull request accidentally matches one of our regex patterns, if so then please consider updating the offending comment (or adding the 'No Trello' PR label).`;
+  `Although the comment ${commentUrl} contained the link https://trello.com/c/${shortLink}, when checking the Trello card we could not find an attachment for pull request ${prUrl}. This can be due to a genuine user error on your behalf, if so then please attach ${prUrl} to https://trello.com/c/${shortLink}. In rarer cases, this step may also fail when a comment in a pull request accidentally matches one of the job's regex patterns, if so then please consider updating the offending comment (or adding the 'No Trello' PR label).`;
 export const ERR_NO_VALID_COMMENTS = () =>
-  `There were no comments in this PR that contained a valid Trello URL. This is likely either intentional or because you forgot to attach this PR a Trello card. In order for this CI check to pass, you need to either attach this PR to a Trello card, or to label your PR with the 'No Trello' label.`;
+  `There were no comments in this PR that contained a valid Trello URL. This is likely either intentional or because you forgot to attach this PR to a Trello card. In order for this CI check to pass, you need to either attach this PR to a Trello card, or to label your PR with the 'No Trello' label.`;
 export const ERR_NO_SHORT_LINK = (description: string) =>
   `Description "${description}" did not contain a valid short link. Please include one like in the following examples: "[abc123] My work description" or "[NOID] My work description".`;
 export const ERR_UNEXPECTED = (error: any) => `Unexpected: ${error}`;
